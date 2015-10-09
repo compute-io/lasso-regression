034f9c2300a487e4ba7d683f8122920ebe105ab4
Planeshifter <pburckhardt@outlook.com>
2015-10-08 23:57:36 -0400

    [UPDATE] minimal working implementation

---
+++ TODO.md
@@ -0,0 +1,7 @@
+TODO
+====
+
+1. Support non-standardized inputs; allow for intercept coefficient
+2. Improve convergence check (evaluate objective function instead of parameter changes?)
+3. Other optimizations?
+4. Add other important data points like residuals to returned object
---
+++ examples/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var matrix = require( 'dstructs-matrix' ),
+	lasso = require( './../lib' ),
+	randomNormal = require( 'distributions-normal-random' ),
+	data,
+	x, y,
+	i;
+
+// Set seed of random number generator:
+randomNormal.seed = 117;
+
+data = randomNormal( 1000 );
+x = matrix( data, [100,10] );
+
+// Generate response variables
+y = new Float64Array( 100 );
+for ( i = 0; i < y.length; i++ ) {
+	y[ i ] = 4 * x.get( i, 3 ) +
+		2 * x.get( i, 5 ) -
+		4 * x.get( i, 7 ) -
+		2 * x.get( i, 9 ) +
+		randomNormal();
+}
+
+console.log( lasso( x, y, 1 ) );
